ec50987ee8e5164d286c779b1c6913ebc273e37d
Сергей Дербенцев <modcom@rambler.ru>
2020-04-06 16:38:41 +0300

    Первая версия

---
+++ src/components/buttonLoadMore.js
@@ -0,0 +1,11 @@
+/**
+ * Создаем разметку Кнопка «Show more»
+ * @return {void}
+ */
+const createButtonLoadMore = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
+
+export {createButtonLoadMore};
